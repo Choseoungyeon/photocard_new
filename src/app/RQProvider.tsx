6bbd46781115422bac27c1ae8c03723b968862dd
Choseoungyeon <bottle34@naver.com>
2025-08-31 13:30:47 +0900

    tanstack query 업데이트

--- src/app/RQProvider.tsx
+++ src/app/RQProvider.tsx
@@ -10,32 +10,33 @@ interface ErrorType extends Error {
   message: string;
 }
 
-export default function Providers({ children }: Props) {
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        refetchOnWindowFocus: false,
-        retryOnMount: true,
-        refetchOnReconnect: false,
-        retry: false,
-        staleTime: 60 * 1000,
-        gcTime: 300 * 1000,
-        throwOnError: (error) => handleError(error),
-      },
-      mutations: {
-        throwOnError: (error) => handleError(error),
-      },
+const handleError = (error: any) => {
+  const typedError = error as ErrorType;
+  // if ([401, 403, 404, 500, 'Network Error'].includes(typedError.resultCode)) {
+  //   throw new Error(`${typedError.message}`);
+  // }
+  return false;
+};
+
+// QueryClient를 컴포넌트 외부에서 생성하여 캐시가 유지되도록 함
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retryOnMount: true,
+      refetchOnReconnect: false,
+      retry: false,
+      staleTime: 60 * 1000,
+      gcTime: 300 * 1000,
+      throwOnError: (error) => handleError(error),
     },
-  });
+    mutations: {
+      throwOnError: (error) => handleError(error),
+    },
+  },
+});
 
-  const handleError = (error: any) => {
-    const typedError = error as ErrorType;
-    // if ([401, 403, 404, 500, 'Network Error'].includes(typedError.resultCode)) {
-    //   throw new Error(`${typedError.message}`);
-    // }
-    return false;
-  };
-
+export default function Providers({ children }: Props) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
